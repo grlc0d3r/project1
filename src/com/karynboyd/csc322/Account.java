78e13ca3c13d6cf8dff424c8b7ffe7a6ef286fe2
karynboyd74 <karynboyd74@gmail.com>
2018-10-28 14:38:38 -0500

    I think it's ready to go

--- src/com/karynboyd/csc322/Account.java
+++ src/com/karynboyd/csc322/Account.java
@@ -1,38 +1,63 @@
 package com.karynboyd.csc322;
 
 import java.util.Date;
+import java.util.Scanner;
 
 public class Account {
-    private int id = 0;
-    private double balance = 0;
-    private double annualInterestRate = .408;
+    private int id;
+    private double balance;
+    private double annualInterestRate;
     private Date dateCreated;
 
-    public Account() {}
+    Scanner input = new Scanner(System.in);
 
-    public Account (int id, double balance, Date dateCreated) {
+    public Account() {
+        this(0,100,0.25d);
+        dateCreated = new Date();
+    }
+
+    public Account (int id, double balance, double annualInterestRate) {
         this.id = id;
         this.balance = balance;
-        this.dateCreated = dateCreated;
+        this.annualInterestRate = annualInterestRate;
+
+        dateCreated = new Date();
     }
 
     public int getID() { return id; }
     public void setID(int id) { this.id = id; }
     public double getBalance() { return balance; }
     public void setBalance(double balance) { this.balance = balance; }
+    public double getAnnualInterestRate() { return annualInterestRate; }
+    public void setAnnualInterestRate(double annualInterestRate ) { this.annualInterestRate = annualInterestRate;}
+    public Date getDateCreate() { return dateCreated; }
 
     public double getMonthlyInterestRate() {
-        double monthlyInterestRate = annualInterestRate / 12;
+        double monthlyInterestRate = annualInterestRate / 1200;
         return monthlyInterestRate;
     }
 
-    public double withdraw(double amount) {
-        if (amount <= balance) {
+    public double withdraw() {
+        double amount;
+        System.out.println("Enter withdrawal amount: ");
+        amount = input.nextDouble();
+        if (amount < balance) {
             balance = balance - amount;
-            return amount;
+        } else {
+            System.out.println("Insufficient Funds.");
         }
-        else {
-            
-        }
+
+        return amount;
     }
+
+    public double deposit() {
+        double amount = input.nextDouble();
+        if (amount <= 0) {
+            System.out.println("Invalid amount entered. Please try again.");
+        } else {
+            balance = balance + amount;
+        }
+        return amount;
+    }
+
 }
--- src/com/karynboyd/csc322/Main.java
+++ src/com/karynboyd/csc322/Main.java
@@ -1,8 +1,42 @@
 package com.karynboyd.csc322;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-    }
-}
+	    Scanner get = new Scanner(System.in);
+	    int accountID;
+	    int selection;
+	    boolean quit = false;
+
+	    Account[] accounts = new Account[10];
+	    for (int i = 0; i < accounts.length; i++)
+        {
+            accounts[i] = new Account();
+        }
+
+	    do {
+            System.out.println("Please enter an account id: ");
+            accountID = get.nextInt();
+            System.out.println("Main menu");
+            System.out.println("1. Check Balance");
+            System.out.println("2. Withdrawal");
+            System.out.println("3. Deposit");
+            System.out.println("4. Exit");
+            selection = get.nextInt();
+
+            switch (selection) {
+                case 1:
+                    System.out.println(accounts[accountID].getBalance());
+                    break;
+                case 2:
+                    accounts[accountID].withdraw();
+                    break;
+                case 3:
+                    accounts[accountID].deposit();
+                    break;
+            }
+        } while (!quit);
+    } // end main()
+} // end class
